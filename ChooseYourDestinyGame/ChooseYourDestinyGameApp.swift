c0927d01b84bb259549424b6c2e5084f4a88f459
burakkurtarir <burak.kurtarir35@hotmail.com>
2024-01-10 12:50:09 +0300

    Initial Commit

---
+++ ChooseYourDestinyGame/ChooseYourDestinyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseYourDestinyGameApp.swift
+//  ChooseYourDestinyGame
+//
+//  Created by Burak Kurtarır on 10.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChooseYourDestinyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChooseYourDestinyGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChooseYourDestinyGame
+//
+//  Created by Burak Kurtarır on 10.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
